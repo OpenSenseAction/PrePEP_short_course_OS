355128c71b95f5ffd1aa5e5e87fda65d69f265af
max <maximilian.graf@kit.edu>
2025-03-16 07:16:27 +0100

    final cosmetics

--- solutions/3_3_1-3_solution.py
+++ solutions/3_3_1-3_solution.py
@@ -5,8 +5,8 @@ for pws_id in  ['ams74', 'ams134', 'ams113', 'ams36', str(ds_pws.isel(id=(ds_pws
     
     ds_pws_hourly.sel(id=pws_id).rainfall.plot(ax=ax[0],label=pws_id)
     (ds_ref.sel(id=distance_matrix_pws_ref.idxmin(dim='id_neighbor').sel(id=pws_id)).rainfall*-1).plot(ax=ax[0],label='-ref')
-    ds_pws.fz_flag.where(ds_pws.fz_flag>0).sel(id=pws_id).plot.line(x='time',lw=3,color='pink',label='FZ',ax=ax[0])
-    ds_pws.hi_flag.where(ds_pws.hi_flag>0).sel(id=pws_id).plot.line(x='time',lw=3,color='cyan',label='HI',ax=ax[0])
+    ds_pws.fz_flag.where(ds_pws.fz_flag>0).sel(id=pws_id).plot.line(x='time',lw=1,color='pink',label='FZ',ax=ax[0])
+    ds_pws.hi_flag.where(ds_pws.hi_flag>0).sel(id=pws_id).plot.line(x='time',lw=1,color='cyan',label='HI',ax=ax[0])
     ax[0].legend()
 
     indcorr_results.plot.scatter(x="dist", y="indcorr", color="grey", alpha=0.5, s=10, ax=ax[1])
@@ -17,5 +17,6 @@ for pws_id in  ['ams74', 'ams134', 'ams113', 'ams36', str(ds_pws.isel(id=(ds_pws
     IndCorr_Score = str(
         "{:.2f}".format(indcorr_results.sel(id_neighbor=pws_id).indcorr_score.values)
     )
+    
     plt.legend()
     plt.show();
